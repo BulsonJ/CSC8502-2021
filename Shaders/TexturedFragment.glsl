29fb20e4da555195152ae4f107b281072af02edc
Jack Bulson <jacktbulson@gmail.com>
2021-11-04 15:28:05 +0000

    Tutorial 8

--- Shaders/TexturedFragment.glsl
+++ Shaders/TexturedFragment.glsl
@@ -1,4 +1,4 @@
-#version  330  core
+#version 330 core
 uniform  sampler2D  diffuseTex;
 uniform  sampler2D  diffuseTex2;
 in  Vertex    {
@@ -7,8 +7,5 @@ in  Vertex    {
 
 out  vec4  fragColour;
 void  main(void)    {
-	if (texture(diffuseTex, IN.texCoord).a >= 1) {
-		discard;
-	}
 	fragColour   = texture(diffuseTex , IN.texCoord );
 }
