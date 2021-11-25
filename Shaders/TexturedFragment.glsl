70992c17dd4ab70bac1cf16de0eb633dac4c0176
Jack Bulson <jacktbulson@gmail.com>
2021-11-25 17:02:22 +0000

    Trying to fix normals on capsule

--- Shaders/TexturedFragment.glsl
+++ Shaders/TexturedFragment.glsl
@@ -1,11 +1,13 @@
 #version 330 core
 uniform  sampler2D  diffuseTex;
-uniform  sampler2D  diffuseTex2;
+
 in  Vertex    {
 	vec2  texCoord;
+	vec3 normal;
 } IN;
 
-out  vec4  fragColour;
+out  vec4  fragColour[2];
 void  main(void)    {
-	fragColour   = texture(diffuseTex , IN.texCoord );
+	fragColour[0]   = texture(diffuseTex , IN.texCoord );
+	fragColour[1] = vec4(IN.normal * 0.5 + 0.5, 1.0);
 }
--- Shaders/TexturedVertex.glsl
+++ Shaders/TexturedVertex.glsl
@@ -4,16 +4,21 @@ uniform  mat4  modelMatrix;
 uniform  mat4  viewMatrix;
 uniform  mat4  projMatrix;
 uniform  mat4  textureMatrix;
+uniform mat3 normalMatrix;
 in   vec3  position;
 in   vec2  texCoord;
 in vec4 colour;
+in vec3 normal;
 
 out  Vertex   {
 	vec2 texCoord;
+	vec3 normal;
 } OUT;
 
 void  main(void){
 	mat4  mvp = projMatrix * viewMatrix * modelMatrix;
 	gl_Position = mvp * vec4(position , 1.0);
+	vec3 wNormal = normalize(normalMatrix * normalize(normal));
+	OUT.normal = wNormal;
 	OUT.texCoord = (textureMatrix * vec4(texCoord , 0.0,  1.0)). xy;
 }
